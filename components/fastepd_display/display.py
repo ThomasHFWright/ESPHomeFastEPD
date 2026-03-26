132d1f7195a32b7ad06c4dbdc87f8630c015b2a7
Thomas Wright <thomas.h.f.wright@gmail.com>
2026-03-26 13:34:43 +0000

    Add minimal FastEPD ESPHome external display component

---
+++ components/fastepd_display/display.py
@@ -0,0 +1,48 @@
+import esphome.codegen as cg
+from esphome.components import display
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+AUTO_LOAD = ["display"]
+
+fastepd_display_ns = cg.esphome_ns.namespace("fastepd_display")
+FastEPDDisplay = fastepd_display_ns.class_(
+    "FastEPDDisplay", cg.PollingComponent, display.DisplayBuffer
+)
+
+CONF_FULL_UPDATE_CLEAR_MODE = "full_update_clear_mode"
+
+FASTEPD_GIT_REF = "1.4.6"
+FASTEPD_REPOSITORY = f"https://github.com/bitbank2/FastEPD.git#{FASTEPD_GIT_REF}"
+
+CLEAR_MODES = {
+    "none": 0,
+    "fast": 1,
+    "slow": 2,
+    "white": 3,
+    "extra_white": 4,
+    "black": 5,
+}
+
+CONFIG_SCHEMA = cv.All(
+    display.FULL_DISPLAY_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(FastEPDDisplay),
+            cv.Optional(CONF_FULL_UPDATE_CLEAR_MODE, default="slow"): cv.enum(
+                CLEAR_MODES, lower=True
+            ),
+        }
+    ).extend(cv.polling_component_schema("never")),
+    cv.only_on_esp32,
+    cv.only_with_arduino,
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await display.register_display(var, config)
+
+    cg.add(var.set_full_update_clear_mode(config[CONF_FULL_UPDATE_CLEAR_MODE]))
+
+    cg.add_library("FastEPD", None, FASTEPD_REPOSITORY)
+    cg.add_library("Wire", None)
---
+++ components/fastepd_display/fastepd_display.cpp
@@ -0,0 +1,46 @@
+#include "fastepd_display.h"
+
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace fastepd_display {
+
+static const char *const TAG = "fastepd_display";
+
+void FastEPDDisplay::setup() {
+  const int rc = this->fastepd_.initPanel(BB_PANEL_M5PAPERS3);
+  if (rc != BBEP_SUCCESS) {
+    ESP_LOGE(TAG, "FastEPD init failed (error=%d)", rc);
+    this->mark_failed();
+    return;
+  }
+
+  this->fastepd_.fillScreen(BBEP_WHITE);
+  this->fastepd_.fullUpdate(this->full_update_clear_mode_, false, nullptr);
+  this->initialized_ = true;
+}
+
+void FastEPDDisplay::update() {
+  if (!this->initialized_) {
+    return;
+  }
+
+  this->do_update_();
+  this->fastepd_.fullUpdate(this->full_update_clear_mode_, false, nullptr);
+}
+
+void FastEPDDisplay::draw_absolute_pixel_internal(int x, int y, Color color) {
+  this->fastepd_.drawPixel(x, y, color.is_on() ? BBEP_WHITE : BBEP_BLACK);
+}
+
+void FastEPDDisplay::dump_config() {
+  LOG_DISPLAY("  ", "FastEPD Display", this);
+  ESP_LOGCONFIG(TAG, "  Panel: M5Stack PaperS3");
+  ESP_LOGCONFIG(TAG, "  Full Update Clear Mode: %u", this->full_update_clear_mode_);
+  LOG_UPDATE_INTERVAL(this);
+}
+
+float FastEPDDisplay::get_setup_priority() const { return setup_priority::PROCESSOR; }
+
+}  // namespace fastepd_display
+}  // namespace esphome
